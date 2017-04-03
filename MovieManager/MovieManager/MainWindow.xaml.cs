5744981589ec04460a36f56652c72abd5aa7521e
manaal1 <Mac@Manaals-MacBook-Pro.local>
2017-04-03 14:48:22 -0400

    Movie Manager

---
+++ MovieManager/MovieManager/MainWindow.xaml.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.OleDb;
+using System.Data;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace MovieManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //List of Movies.
+        ObservableCollection<Movie> listOfMovie = new ObservableCollection<Movie>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            gridBody.Visibility = System.Windows.Visibility.Hidden;
+            loadTitles();                        
+        }
+
+        //Load Combobox Movie Title.
+        private void loadTitles()
+        {
+            cmbxTitle.ItemsSource = null;
+            listOfMovie.Clear();
+            Movie.ReadRecords(listOfMovie);
+
+            if (listOfMovie.Count > 0)
+            {
+                cmbxTitle.ItemsSource = listOfMovie;
+                cmbxTitle.DisplayMemberPath = "Title";
+                cmbxTitle.SelectedValuePath = "ID";
+            }
+        }
+
+        //Reset all fields for new entry.
+        private void clearFields()
+        {
+            lblID.Content = "";
+            txtDesc.Text = "";
+            txtGenre.Text = "";
+            txtPicPath.Text = "";
+            txtRating.Text = "";
+            txtRDate.Text = "";
+            txtTitle.Text = "";
+            txtBoxOfficeColl.Text = "";
+            imgMoviePic.Source = null;
+        }
+
+        //Load a selected movie.
+        private void cmbxTitle_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                if (cmbxTitle.SelectedIndex >= 0)
+                {
+                    clearFields();
+
+                    gridBody.Visibility = System.Windows.Visibility.Visible;
+
+                    Movie selectedMovie = (Movie)cmbxTitle.Items[cmbxTitle.SelectedIndex];
+
+                    lblID.Content = selectedMovie.ID.ToString();
+                    txtTitle.Text = selectedMovie.Title;
+                    txtGenre.Text = selectedMovie.Genre;
+                    txtRDate.Text = selectedMovie.ReleaseDate.ToShortDateString();
+                    chbxSold.IsChecked = (bool)selectedMovie.Sold;
+                    txtDesc.Text = selectedMovie.Description;
+                    txtPicPath.Text = selectedMovie.PicturePath;
+                    txtRating.Text = selectedMovie.Rating.ToString();
+                    txtBoxOfficeColl.Text = string.Format("{0:0.00}", selectedMovie.BoxofficeColl);
+
+                    var uri = new Uri(selectedMovie.PicturePath);
+                    var bitmap = new BitmapImage(uri);
+                    imgMoviePic.Source = bitmap;
+                }
+                else
+                {
+                    gridBody.Visibility = System.Windows.Visibility.Hidden;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + Environment.NewLine + "Load failed.", "Error!");
+            }
+            
+        }
+
+        //Make all GUI fields clear for add new entry.
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                cmbxTitle.SelectedIndex = -1;
+                gridBody.Visibility = System.Windows.Visibility.Visible;
+                clearFields();               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!");
+            }
+        }
+
+        //Delete a selected movie and update the list.
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (cmbxTitle.SelectedIndex >= 0)
+                {
+                    Movie selectedMovie = (Movie)cmbxTitle.Items[cmbxTitle.SelectedIndex];
+
+                    Movie.deleteMovie(selectedMovie);
+
+                    loadTitles();
+                }
+                else
+                {
+                    MessageBox.Show("Please select a movie for delete.", "Information!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + Environment.NewLine + "Data has not Deleted successfully.", "Error!");
+            }            
+        }
+
+        //Refresh the window.
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            gridBody.Visibility = System.Windows.Visibility.Hidden;
+            cmbxTitle.SelectedIndex = -1;
+            clearFields();
+        }
+
+        //Get all user input and after validate store into Movie object
+        //and save it to database table.
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Movie newMovie = null;
+                bool isNew = false;
+
+                if (string.IsNullOrEmpty(lblID.Content.ToString()))
+                {
+                    //Add New Movie...
+                    newMovie = new Movie();
+                    isNew = true;
+                }
+                else
+                {
+                    //Update the selected Movie...
+                    newMovie = (Movie)cmbxTitle.Items[cmbxTitle.SelectedIndex];
+                    isNew = false;
+                }
+
+                newMovie.Title = txtTitle.Text;
+                newMovie.Genre = txtGenre.Text;
+                DateTime rDate = new DateTime();
+
+                if (DateTime.TryParse(txtRDate.Text, out rDate))
+                {
+                    newMovie.ReleaseDate = rDate;
+                }
+                else
+                {
+                    MessageBox.Show("Release Date string is not in correct format.", "Invaild Input");
+                    return;
+                }
+                newMovie.Sold = Convert.ToBoolean(chbxSold.IsChecked);
+                newMovie.Description = txtDesc.Text;
+                newMovie.PicturePath = txtPicPath.Text;
+
+                int rating = 0;
+
+                if (int.TryParse(txtRating.Text, out rating))
+                {
+                    newMovie.Rating = rating;
+                }
+                else
+                {
+                    MessageBox.Show("Rating should be numeric value.", "Invaild Input");
+                    return;
+                }
+
+                decimal boxofficeColl = 0;
+
+                if (decimal.TryParse(txtBoxOfficeColl.Text, out boxofficeColl))
+                {
+                    newMovie.BoxofficeColl = boxofficeColl;
+                }
+                else
+                {
+                    MessageBox.Show("Box Office Collection should be decimal value.", "Invaild Input");
+                    return;
+                }
+
+                Movie.addEditMovie(newMovie, isNew);
+
+                loadTitles();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + Environment.NewLine + "Data has not saved successfully.", "Error!");
+            }
+        }
+
+        //Selete a picture by openig file location
+        //and convert it to BitmapImage and display.
+        private void btnSelectPicture_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure dialog box
+            System.Windows.Forms.OpenFileDialog dlg = new System.Windows.Forms.OpenFileDialog();
+            
+            dlg.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png, *.bmp) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png; *.bmp"; // Filter files by extension
+
+            // Show open file dialog box
+            System.Windows.Forms.DialogResult result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                // Open document
+                txtPicPath.Text = dlg.FileName;
+
+                var uri = new Uri(txtPicPath.Text);
+                var bitmap = new BitmapImage(uri);
+                imgMoviePic.Source = bitmap;
+            }
+        }
+    }
+}
---
+++ MovieManager/MovieManager/Movie.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MovieManager
+{
+    /// <summary>
+    /// Create a Movie class for store database data into runtime
+    /// and manage load, add, edit and delete through this class.
+    /// This class has all fields same as database 'MOVIE' table.
+    /// </summary>
+    class Movie
+    {
+        //Declare a private string field for store the database connection.
+        private static OleDbConnection conn = new OleDbConnection(
+                    "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Movie.accdb;Persist Security Info=True");
+        private int id;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private string genre;
+
+        public string Genre
+        {
+            get { return genre; }
+            set { genre = value; }
+        }
+
+        private DateTime releasedate;
+
+        public DateTime ReleaseDate
+        {
+            get { return releasedate; }
+            set { releasedate = value; }
+        }
+
+        private bool sold;
+
+        public bool Sold
+        {
+            get { return sold; }
+            set { sold = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private string picturePath;
+
+        public string PicturePath
+        {
+            get { return picturePath; }
+            set { picturePath = value; }
+        }
+
+        private int rating;
+
+        public int Rating
+        {
+            get { return rating; }
+            set { rating = value; }
+        }
+
+        private decimal boxofficeColl;
+
+        public decimal BoxofficeColl
+        {
+            get { return boxofficeColl; }
+            set { boxofficeColl = value; }
+        }
+        
+
+        /// <summary>
+        /// Read all data from database and load it into given parameter list object.
+        /// </summary>
+        /// <param name="listOfMovie">List of Movies</param>
+        public static void ReadRecords(ObservableCollection<Movie> listOfMovie)
+        {
+            OleDbDataReader reader = null;
+            try
+            {
+                //Open DB connection.
+                conn.Open();
+
+                //Query for select all data of 'MOVIE' table from database.
+                OleDbCommand cmd = new OleDbCommand("Select * FROM MOVIE", conn);
+
+                //Execute query through OleDBDataReader.
+                reader = cmd.ExecuteReader();
+
+                //Till reader has data.
+                while (reader.Read())
+                {
+                    //Create a Movie object from each row of DB table 'MOVIE'.
+                    Movie m = new Movie();
+
+                    m.ID = Convert.ToInt32(reader.GetValue(0).ToString());
+                    m.Title = reader.GetString(1);
+                    m.Genre = reader.GetString(2);
+                    m.ReleaseDate = Convert.ToDateTime(reader.GetValue(3).ToString());
+                    m.Sold = Convert.ToBoolean(reader.GetValue(4).ToString());
+                    m.Description = reader.GetString(5);
+                    m.PicturePath = reader.GetValue(6).ToString();
+                    m.Rating = Convert.ToInt32(reader.GetValue(7).ToString());
+                    m.BoxofficeColl = Convert.ToDecimal(reader.GetValue(8).ToString());
+
+                    //Add movie object to list.
+                    listOfMovie.Add(m);
+                }
+            }
+            catch (Exception e)
+            {
+                //If any connection related or other exception happen, show this message.
+                MessageBox.Show(e.Message + Environment.NewLine + "Connection has not open successfully.", "Error!");
+            }
+            finally
+            {
+                //when all done close the reader and connection.
+                if (reader != null)
+                {
+                    reader.Close();
+                    conn.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Take all data from given parameter Movie object and make a
+        /// query string for Insert if 'isNew' else Update.
+        /// Run query and change the database data.
+        /// </summary>
+        /// <param name="newMovie">New Movie object if 'isNew' else selected Movie object.</param>
+        /// <param name="isNew">New Movie for add or Previous Movie for update</param>
+        public static void addEditMovie(Movie newMovie, bool isNew)
+        {
+            try
+            {
+                conn.Open();
+
+                OleDbCommand cmd = new OleDbCommand();
+                cmd.CommandType = CommandType.Text;
+
+                if (isNew)
+                {
+                    //Insert query for add new Movie.
+                    cmd.CommandText = "insert into MOVIE ([TITLE], [GENRE], [RELEASE_DATE], [SOLD], [DESCRIPTION], [PICTURE_PATH], [RATING], [BOXOFFICE_COLL]) " +
+                        "values (@TITLE, @GENRE, @RELEASE_DATE, @SOLD, @DESCRIPTION, @PICTURE_PATH, @RATING, @BOXOFFICE_COLL)";
+                    cmd.Parameters.AddWithValue("@TITLE", newMovie.Title);
+                    cmd.Parameters.AddWithValue("@GENRE", newMovie.Genre);
+                    cmd.Parameters.AddWithValue("@RELEASE_DATE", newMovie.ReleaseDate);
+                    cmd.Parameters.AddWithValue("@SOLD", newMovie.Sold);
+                    cmd.Parameters.AddWithValue("@DESCRIPTION", newMovie.Description);
+                    cmd.Parameters.AddWithValue("@PICTURE_PATH", newMovie.PicturePath);
+                    cmd.Parameters.AddWithValue("@RATING", newMovie.Rating);
+                    cmd.Parameters.AddWithValue("@BOXOFFICE_COLL", newMovie.BoxofficeColl);
+                }
+                else
+                {
+                    //Update query for edit selected Movie.
+                    cmd.CommandText = "update MOVIE set [TITLE] = @TITLE, [GENRE] = @GENRE, [RELEASE_DATE] = @RELEASE_DATE, " +
+                        "[SOLD] = @SOLD, [DESCRIPTION] = @DESCRIPTION, [PICTURE_PATH] = @PICTURE_PATH, [RATING] = @RATING " +
+                        ", [BOXOFFICE_COLL] = @BOXOFFICE_COLL where [ID] = " + newMovie.ID;
+                    cmd.Parameters.AddWithValue("@TITLE", newMovie.Title);
+                    cmd.Parameters.AddWithValue("@GENRE", newMovie.Genre);
+                    cmd.Parameters.AddWithValue("@RELEASE_DATE", newMovie.ReleaseDate);
+                    cmd.Parameters.AddWithValue("@SOLD", newMovie.Sold);
+                    cmd.Parameters.AddWithValue("@DESCRIPTION", newMovie.Description);
+                    cmd.Parameters.AddWithValue("@PICTURE_PATH", newMovie.PicturePath);
+                    cmd.Parameters.AddWithValue("@RATING", newMovie.Rating);
+                    cmd.Parameters.AddWithValue("@BOXOFFICE_COLL", newMovie.BoxofficeColl);
+                }
+
+                cmd.Connection = conn;
+                cmd.ExecuteNonQuery();
+
+                if (isNew)
+                {
+                    MessageBox.Show("A New Movie has been successfully added.", "Add New!");
+                }
+                else
+                {
+                    MessageBox.Show("A Movie has been successfully updated.", "Update!");
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error!");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        /// <summary>
+        /// Delete selected Movie which has passed by parameter.
+        /// </summary>
+        /// <param name="selectedMovie">The selected Movie for delete</param>
+        public static void deleteMovie(Movie selectedMovie)
+        {
+            try
+            {
+                conn.Open();
+
+                OleDbCommand cmd = new OleDbCommand();
+                cmd.CommandType = CommandType.Text;
+
+                //Delete query for delete the selected Movie.
+                cmd.CommandText = "delete from MOVIE where [ID] = " + selectedMovie.ID;
+                
+                cmd.Connection = conn;
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Selected Movie has been successfully deleted.", "Delete!");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error!");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+    }
+}
